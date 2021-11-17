46f421977cbe34a136ad9bb04e2649ade3295844
Rizkyka Mediano Sandie <medianosandie@gmail.com>
2021-11-17 14:52:09 +0700

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <link href=" {{ mix('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <h1>selamat datang</h1>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <i class="fas fa-address-card"></i>
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html>
