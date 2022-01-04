c97bc5cb08c7afe85b995065cba111fb1f128758
Rizkyka Mediano Sandie <medianosandie@gmail.com>
2022-01-04 21:35:43 +0700

    adding head and experience section

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,7 +1,8 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Anggaku</title>
-<link rel="icon" type="image/x-icon" href="{{ asset('img/mylogo.jpg') }}">
+<title>{{config('app.name')}}</title>
+<link rel="icon" type="image/x-icon" href="https://avatars.githubusercontent.com/medianosandie">
 <!-- Styles -->
 <link href=" {{ mix('css/app.css') }}" rel="stylesheet">
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
--- resources/views/pages/header.blade.php
+++ resources/views/pages/header.blade.php
@@ -1,1 +1,13 @@
-<h1 class="text-danger">ini adalah section header</h1>
+<header class="masthead">
+    <div class="container">
+        <div class="row align-items-center row-title">
+            <div class="col-sm-3 text-center" data-aos="fade-right" data-aos-duration="1500">
+                <img src="https://avatars.githubusercontent.com/medianosandie" class="img-fluid my-photo" alt="my-photo">
+            </div>
+            <div class="col-sm-9 text-center" data-aos="fade-up" data-aos-duration="1500">
+                <div class="my-name">Hi, I'm <strong>Sandie</strong></div>
+                <div class="caption">Student</div>
+            </div>
+        </div>
+    </div>
+</header>
